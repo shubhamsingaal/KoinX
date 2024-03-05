fd0c8dac009bc94c1e28eb3ae6cbfef2e37a3c97
Shubham Kumar <shubhamkraj200@gmail.com>
2024-03-05 23:06:11 +0530

    feat:performance scale

--- src/components/Crypto.tsx
+++ src/components/Crypto.tsx
@@ -33,7 +33,7 @@ function Crypto(): JSX.Element {
   }, []);
 
   return (
-    <div className="bg-white h-max rounded-lg ml-14 my-4 p-6">
+    <div className="bg-white h-max rounded-lg my-4 p-6">
       <div className="flex items-center">
         <div>
           <img src={img} className="w-9" alt="Bitcoin" />
@@ -87,7 +87,7 @@ function Crypto(): JSX.Element {
           <div>All</div>
         </div>
       </div>
-      <div className="h-[400px]">
+      <div className="h-[420px]">
         <TradingViewWidget />
       </div>
     </div>
