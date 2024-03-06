23591d6a4ebbe9687252f00a3e165a55da53fd3d
Shubham Kumar <shubhamkraj200@gmail.com>
2024-03-06 18:15:45 +0530

    feat: mobile view & sentiments section

--- src/components/Crypto.tsx
+++ src/components/Crypto.tsx
@@ -33,7 +33,7 @@ function Crypto(): JSX.Element {
   }, []);
 
   return (
-    <div className="bg-white h-max rounded-lg my-4 p-6">
+    <div className="bg-white h-max rounded-lg my-5 p-6">
       <div className="flex items-center">
         <div>
           <img src={img} className="w-9" alt="Bitcoin" />
@@ -42,7 +42,7 @@ function Crypto(): JSX.Element {
           Bitcoin
         </div>
         <div className="text-sm text-[#5D667B] pl-2">BTC</div>
-        <div className="bg-[#808A9D] px-3 py-2 text-white rounded-xl ml-7">
+        <div className="bg-[#808A9D] px-3 py-2 text-white rounded-lg ml-7">
           Rank #1
         </div>
       </div>
@@ -55,26 +55,44 @@ function Crypto(): JSX.Element {
             {(cryptoData && `₹ ${cryptoData.inr}`) || `₹ 5535287`}
           </div>
         </div>
-        <div className="flex items-center justify-center bg-green-300/20 rounded-lg p-2 h-10 ml-10">
+        <div
+          className={`flex items-center justify-center rounded-lg p-2 h-10 ml-10 ${
+            cryptoData && cryptoData.inr_24h_change < 0
+              ? "bg-red-300/20"
+              : "bg-green-300/20"
+          }`}
+        >
           <svg
             viewBox="0 0 100 100"
-            className="w-4 fill-current text-green-600"
+            className={`w-4 fill-current ${
+              cryptoData && cryptoData.inr_24h_change < 0
+                ? "text-red-600 rotate-180"
+                : "text-green-600"
+            }`}
           >
             <polygon points="0,100 50,0 100,100" />
           </svg>
-          <span className="ml-2 text-sm font-bold text-green-600">
-            {(cryptoData && `${cryptoData.inr_24h_change.toFixed(2)}%`) ||
+          <span
+            className={`ml-2 text-sm font-bold ${
+              cryptoData && cryptoData.inr_24h_change < 0
+                ? "text-red-600"
+                : "text-green-600"
+            }`}
+          >
+            {(cryptoData &&
+              `${Math.abs(cryptoData.inr_24h_change).toFixed(2)}%`) ||
               `2.18%`}
           </span>
         </div>
+
         <div className="text-sm text-[#768396] ml-2 mt-2">(24H)</div>
       </div>
       <hr className="my-4" />
-      <div className="flex mb-4 justify-between">
-        <div className="text-lg font-semibold text-[#0B1426]">
+      <div className="lg:flex mb-4 justify-between">
+        <div className="ls:text-lg text-sm font-semibold text-[#0B1426]">
           Bitcoin Price Chart (USD)
         </div>
-        <div className="flex space-x-5 mr-4 text-sm text-[#5D667B] font-medium text-center items-center">
+        <div className="flex lg:space-x-5 space-x-3 mr-4 text-sm text-[#5D667B] font-medium text-center items-center">
           <div>1H</div>
           <div>24H</div>
           <div className="text-[#0141CF] bg-[#E2ECFE] rounded-3xl px-3 py-1">
@@ -87,7 +105,7 @@ function Crypto(): JSX.Element {
           <div>All</div>
         </div>
       </div>
-      <div className="h-[420px]">
+      <div className="lg:h-[420px] h-[300px]">
         <TradingViewWidget />
       </div>
     </div>
--- src/pages/CoinPage.tsx
+++ src/pages/CoinPage.tsx
@@ -1,26 +1,36 @@
+import About from "../components/About";
 import CradsPromo from "../components/CradsPromo";
 import Crypto from "../components/Crypto";
 import PageHolder from "../components/PageHolder";
 import PerformanceSection from "../components/PerformanceSection";
+import SentimentSection from "../components/SentimentSection";
 import ToggleSection from "../components/ToggleSection";
+import Tokenomics from "../components/Tokenomics";
 import TrendingCoins from "../components/TrendingCoins";
+import Team from "../components/TeamCard";
+import SuggestionSection from "../components/SuggestionSection";
 
 function CoinPage() {
   return (
     <div className="h-full w-screen bg-slate-200/40">
       <PageHolder />
 
-      <div className="w-screen flex">
-        <div className="w-8/12 ml-14">
+      <div className="w-screen lg:flex">
+        <div className="lg:w-8/12 lg:ml-14 mx-4">
           <Crypto />
           <ToggleSection />
           <PerformanceSection />
+           <SentimentSection/>
+             <About/>
+             <Tokenomics/>
+             <Team/> 
         </div>
-        <div className="w-4/12 mr-14">
-          <CradsPromo />
-          <TrendingCoins />
-        </div>
+         <div className="lg:w-4/12 lg:mr-14 mx-4">
+          <CradsPromo/>
+          <TrendingCoins/>
+        </div> 
       </div>
+      <SuggestionSection/> 
     </div>
   );
 }
